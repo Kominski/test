58fcdafeee30dee583c99e9aee24ac2b12439b04
esparreboom <eveline@kominski.net>
2013-10-01 18:34:30 +0200

    message from commandline

--- index.php
+++ index.php
@@ -4,5 +4,5 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-echo "hello world enxo phingiephing commitpush";
+echo "hello world enxo phingiephing commitpush custom msg";
 ?>
